70187fd26f3486ceba2e4e41ad25144f98fa0e8d
hipstermartin <aamilabdulla0@gmail.com>
2025-12-05 19:41:11 +0400

    ..

--- src/text_summarizer/pipeline/prediction.py
+++ src/text_summarizer/pipeline/prediction.py
@@ -3,15 +3,19 @@ from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 
 class PredictionPipeline:
     def __init__(self):
-        # project root = folder containing app.py
         project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../../"))
 
         model_dir = os.path.join(project_root, "artifacts/model_trainer/t5-summarizer")
         tokenizer_dir = os.path.join(project_root, "artifacts/model_trainer/tokenizer")
 
-        # Load tokenizer and model from local saved folders
+        print("MODEL PATH:", model_dir)
+        print("TOKENIZER PATH:", tokenizer_dir)
+        print("MODEL EXISTS:", os.path.exists(model_dir))
+        print("TOKENIZER EXISTS:", os.path.exists(tokenizer_dir))
+
         self.tokenizer = AutoTokenizer.from_pretrained(tokenizer_dir, local_files_only=True)
         self.model = AutoModelForSeq2SeqLM.from_pretrained(model_dir, local_files_only=True)
+
 
     def predict(self, text):
         if isinstance(text, list):
